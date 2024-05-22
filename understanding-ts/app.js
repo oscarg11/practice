2ae63e4d8c25b3d51b75d2d929a80b3509d3d113
oscarg11 <oscar.g2511@gmail.com>
2024-05-22 18:47:42 -0400

    object types lesson

--- understanding-ts/app.js
+++ understanding-ts/app.js
@@ -1,15 +1,9 @@
-function add(n1, n2, showResult, phrase) {
-    console.log(typeof n1);
-    var result = n1 + n2;
-    if (showResult) {
-        console.log(phrase + result);
-    }
-    else {
-        return result;
-    }
-}
-var number1 = 5;
-var number2 = 2.8;
-var printResult = true;
-var resultPhrase = "Result is: ";
-add(number1, number2, printResult, resultPhrase);
+// const person: {
+//     name : string;
+//     age: number;
+// } 
+var person = {
+    name: 'Oscar',
+    age: 25
+};
+console.log(person.name);
