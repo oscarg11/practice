d7ae7da54e13d478f6b2bbfa33a6fff09970302a
oscarg11 <oscar.g2511@gmail.com>
2024-05-22 23:11:36 -0400

    Practice with array types and tuples

--- understanding-ts/app.js
+++ understanding-ts/app.js
@@ -4,6 +4,15 @@
 // } 
 var person = {
     name: 'Oscar',
-    age: 25
+    age: 25,
+    hobbies: ['sports', 'cooking'],
+    role: [2, 'author']
 };
+person.role.push('admin');
+var favoriteActivities;
+favoriteActivities = ['sports'];
 console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+}
--- understanding-ts/app.ts
+++ understanding-ts/app.ts
@@ -2,9 +2,27 @@
 //     name : string;
 //     age: number;
 // } 
-const person = {
+const person: {
+    name: string;
+    age: number;
+    hobbies: string[];
+    role: [number, string]
+} = {
     name : 'Oscar',
-    age: 25
+    age: 25,
+    hobbies: ['sports', 'cooking'],
+    role: [2, 'author']
 };
 
+//person.role.push('admin');
+person.role = [0, 'admin', 'user'];
+
+let favoriteActivities: string[];
+favoriteActivities = ['sports'];
+
 console.log(person.name);
+
+for (const hobby of person.hobbies){
+    console.log(hobby.toUpperCase())
+
+}
