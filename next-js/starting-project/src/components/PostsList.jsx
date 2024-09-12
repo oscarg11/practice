1439bbc6293a133e41df0616990fd2b3894d7b7e
oscarg11 <oscar.g2511@gmail.com>
2024-09-11 23:17:33 -0400

    learning about state and conditional content

--- next-js/starting-project/src/components/PostsList.jsx
+++ next-js/starting-project/src/components/PostsList.jsx
@@ -1,9 +1,11 @@
 import React, {useState} from 'react';
 import NewPost from "./NewPost";
 import Post from "./Post";
+import Modal from "./Modal"
 import classes from'./PostsList.module.css';
 
 function PostsList(){
+    const [ modalIsVisible, setModalIsVisible ]= useState(true);
     const [enteredBody, setEnteredBody] = useState(''); // Body text state
     const [enteredAuthor, setEnteredAuthor]=useState('');// Author text state
 
@@ -17,9 +19,20 @@ function PostsList(){
         console.log(event.target.value);
         setEnteredAuthor(event.target.value);
     }
+    //Modal hide function
+    function hideModalHandler(){
+        setModalIsVisible(false);
+    }
     return (
     <>
-        <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler} />
+        {modalIsVisible ? (
+        <Modal onClose={hideModalHandler}>
+            <NewPost
+                onBodyChange={bodyChangeHandler}
+                onAuthorChange={authorChangeHandler}
+            />
+        </Modal>
+        ) : null}
         <ul className={classes.posts}>
             <Post author={enteredAuthor} body={enteredBody}/>
             <Post author={enteredAuthor} body={enteredBody}/>
