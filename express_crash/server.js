b8ace87ef1059f8a009464576a5f35328feb273a
oscarg11 <oscar.g2511@gmail.com>
2024-12-30 11:49:40 -0500

    completed express crash course

--- express_crash/server.js
+++ express_crash/server.js
@@ -1,9 +1,16 @@
 import express from 'express'
-
 import path from 'path';
+import { fileURLToPath } from 'url'
 import posts from './routes/posts.js'
 import logger from './middleware/logger.js'
+import errorHandler from './middleware/error.js';
+import notFound from './middleware/notFound.js';
 const port = process.env.PORT || 8000;
+
+//get the diretory name
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename)
+console.log(__filename)
 
 const app = express();
 
@@ -15,11 +22,16 @@ app.use(express.urlencoded({extended: false}))
 app.use(logger);
 
 //setup static folder
-//app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')))
 
 
 //Routes
 app.use('/api/posts',posts);
+
+
+// ERROR HANDLER
+app.use(errorHandler);
+app.use(notFound);
 
 
 
