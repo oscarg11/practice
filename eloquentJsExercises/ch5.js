3b96f67de33053262acd2d21b06ca8112839babd
oscarg11 <oscar.g2511@gmail.com>
2025-05-30 17:39:46 -0400

    completed exercise 3 every

--- eloquentJsExercises/ch5.js
+++ eloquentJsExercises/ch5.js
@@ -6,6 +6,7 @@ Use the reduce method in combination with the concat method to "flatten"
 an array of arrays into a single array that contains all the elements of
 the original arrays.
 */
+console.log("1.) Flattening")
 const arrayOfArrays = [
     [1,2,3,4],
     ['a', 'b', 'c', 'd']
@@ -30,7 +31,7 @@ Finally, it calls the update function to create a new value and starts from the 
 
 When defining the function, you can use a regular loop to do the actual looping.
 */
-
+console.log("2.) Your Own Loop")
 //where to stop the loop
 function test(n){
     return n < 10;
@@ -57,3 +58,38 @@ function customLoop(value, test, update, body){
     
 }
 console.log(customLoop(0, test, update, body));
+
+/**
+3. Everything
+
+Analogous to the some method, arrays also have an every method. This one 
+returns true when the given function returns true for every element in the array.
+In a way, some is a version of the || operator, and every is a version of the && operator.
+
+Implement every as a function that takes an array and a predicate function.
+ */
+
+console.log("3.) Every")
+
+function isPositive(n){
+    return n > 0;
+}
+
+function every(arr, isPositive){
+    for(let i = 0; i < arr.length; i++){
+        if(!isPositive(arr[i])){
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log(every([1,2,3,4], isPositive));
+
+console.log("using the built in function every():")
+//Heres how it would look like using the build in function every()
+const numbers = [1,2,3,4];
+
+const allPositiveNumbers = numbers.every(n => n > 0);
+
+console.log(allPositiveNumbers);
